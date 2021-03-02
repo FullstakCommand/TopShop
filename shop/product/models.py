3f51268dd03012779c8c3f77a51e055480d55c9e
Evgeny <eugen.eisner@gmail.com>
2021-03-02 17:08:37 +0100

    update product

--- shop/product/models.py
+++ shop/product/models.py
@@ -12,18 +12,26 @@ class Category(models.Model):
         return self.name
 
 
+def my_attrs():
+    return {
+        'foo': 'bar'
+    }
+
+
 class Product(models.Model):
     category = models.ManyToManyField(Category, verbose_name='категория')
     name = models.CharField('название товара', max_length=120)
     description = models.TextField('описание')
     item_number = models.BigIntegerField('номер товара', default=0)
     price = models.FloatField('цена', default=0.00)
+    image = models.ImageField('фото', upload_to='media')
+    set_attrs = models.JSONField(default=my_attrs)
 
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
 
     def __str__(self):
-        return self.name
+        return '%s %s %s ' % (self.name, self.item_number, self.price)
 
 
